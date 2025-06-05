478fe86160e44645c54fe15167bebd4105f1aacc
Ilverism <72947008+Ilverism@users.noreply.github.com>
2025-06-05 05:00:01 -0400

    Updates

---
+++ _app/immutable/nodes/0.BpYOPPK7.js
@@ -0,0 +1,1 @@
+import{c as a,a as n}from"../chunks/BIHuWR4G.js";import{o as s}from"../chunks/CYnANuKR.js";import{s as p}from"../chunks/BYcs1MQN.js";const i=!1,f=Object.freeze(Object.defineProperty({__proto__:null,prerender:i},Symbol.toStringTag,{value:"Module"}));function d(o,r){var e=a(),t=s(e);p(t,()=>r.children),n(o,e)}export{d as component,f as universal};
