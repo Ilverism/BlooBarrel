a5914a77eafe70b2d993a581360d9ccf4d7be0a9
Ilverism <72947008+Ilverism@users.noreply.github.com>
2025-06-05 04:40:13 -0400

    Update root page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,13 +2,20 @@
 
     //Automatically redirecto to the /releases page
     import { onMount } from 'svelte';
+    import { base } from '$app/paths';
+
+    const redirectURL = `${base}/releases`;
 
     onMount(() => {
-        window.location.href = '/releases';
+
+        //Wait for 10 seconds before redirecting
+        setTimeout(() => {
+            window.location.href = redirectURL;
+        }, 10_000);
+
     });
 
 </script>
 
 <h4>Welcome to BlooBarrel</h4>
-<h1>Moving to releases page...</h1>
-
+<h1>Moving to releases page momentarily... (Redirecting to <a href={redirectURL}>'{redirectURL}'</a>)</h1>
